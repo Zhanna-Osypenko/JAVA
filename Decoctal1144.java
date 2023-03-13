ed26caff3023ceddc054205c91fc479ea5aa7ba0
Zhanna Osypenko <106883444+Zhanna-Osypenko@users.noreply.github.com>
2023-03-13 22:20:12 +0100

    HW1_number_1144
    HW change system 10 => 8 number 1144

---
+++ Decoctal1144.java
@@ -0,0 +1,21 @@
+public class Decoctal1144 {
+    public static void main(String[] args) {
+        int a = 1144;
+        System.out.println(1144/8);
+        System.out.println("ostatok = " + (1144%8));
+
+        System.out.println(143/8);
+        System.out.println("ostatok = " + (143%8));
+
+        System.out.println(17/8);
+        System.out.println("ostatok = " + (17%8));
+
+        System.out.println(2/8);
+        System.out.println("ostatok = " + (2%8));
+
+        int a8 = 02170;
+        System.out.println("2170 from 8 to 10 system = " + a8);
+
+
+    }
+}
