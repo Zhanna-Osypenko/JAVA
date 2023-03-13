0abaf827abc3aef79ebcea7473be54d5fea8e53f
Zhanna Osypenko <106883444+Zhanna-Osypenko@users.noreply.github.com>
2023-03-13 22:19:17 +0100

    HW1_number_978
    HW change system 10 => 8 number 978

---
+++ Decoctal978.java
@@ -0,0 +1,21 @@
+public class Decoctal978 {
+    public static void main(String[] args) {
+        int a = 978;
+        System.out.println(978/8);
+        System.out.println("ostatok = " + (978%8));
+
+        System.out.println(122/8);
+        System.out.println("ostatok = " + (122%8));
+
+        System.out.println(15/8);
+        System.out.println("ostatok = " + (15%8));
+
+        System.out.println(1/8);
+        System.out.println("ostatok = " + (1%8));
+
+        int a8 = 01722;
+        System.out.println("1722 from 8 to 10 system = " + a8);
+
+
+    }
+}
