fdc5bac0c2d7f52f4a7618b66ce815466429a233
Zhanna Osypenko <106883444+Zhanna-Osypenko@users.noreply.github.com>
2023-03-13 22:17:13 +0100

    HW1_Number_12
    HW change system 10 => 8 number 12

---
+++ Decoctal12.java
@@ -0,0 +1,15 @@
+public class Decoctal12 {
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println(12/8);
+        System.out.println("ostatok = " + (12%8));
+
+        System.out.println(1/8);
+        System.out.println("ostatok = " + (1%8));
+
+        int a8 = 014;
+        System.out.println("14 from 8 to 10 system = " + a8);
+
+
+    }
+}
