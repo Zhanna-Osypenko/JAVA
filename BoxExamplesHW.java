0cc477d7165fa710a19ec3f660d741d90daa0df6
Zhanna Osypenko <106883444+Zhanna-Osypenko@users.noreply.github.com>
2023-03-15 21:51:57 +0100

    HW4_BoxExamples
    BoxExamples

---
+++ BoxExamplesHW.java
@@ -0,0 +1,15 @@
+public class BoxExamplesHW {
+    public static void main(String[] args) {
+        com.auto.finland.Box Box1 = new com.auto.finland.Box(2,5,10);
+
+        com.auto.finland.Box Box2 =
+                new com.auto.finland.Box(6,4);
+
+        com.auto.finland.Box Box3 =
+                new com.auto.finland.Box(20);
+
+        com.auto.finland.Box Box4 =
+                new com.auto.finland.Box();
+
+    }
+}
