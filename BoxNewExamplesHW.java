ee0dd1594cf38ceb46ec5dee2fce72aa44825b76
Zhanna Osypenko <106883444+Zhanna-Osypenko@users.noreply.github.com>
2023-03-18 21:02:38 +0100

    HW5_BoxNewExamples
    BoxNew using "this"

---
+++ BoxNewExamplesHW.java
@@ -0,0 +1,19 @@
+public class BoxNewExamplesHW {
+    public static void main(String[] args) {
+        com.auto.finland.BoxNew Box1 =
+                new com.auto.finland.BoxNew(2,5,10);
+        System.out.println("-----------------------");
+
+        com.auto.finland.BoxNew Box2 =
+                new com.auto.finland.BoxNew(6,4);
+        System.out.println("-----------------------");
+
+        com.auto.finland.BoxNew Box3 =
+                new com.auto.finland.BoxNew(20);
+        System.out.println("-----------------------");
+
+        com.auto.finland.BoxNew Box4 =
+                new com.auto.finland.BoxNew();
+
+    }
+}
